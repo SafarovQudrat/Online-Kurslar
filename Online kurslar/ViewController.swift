e30a5d97b76db1faab551741d7782631b67a7cba
qudrat <qudratsafarov@icloud.com>
2024-01-31 16:46:10 +0500

    Initial Commit

---
+++ Online kurslar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Online kurslar
+//
+//  Created by MacBook Pro on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
